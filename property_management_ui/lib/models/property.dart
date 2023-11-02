fcad0bf5f57b9c4edd5ef996d4da0fc5d0de376a
Juan Alegría <juanszalegria@gmail.com>
2023-11-02 16:51:22 -0500

    Feat: add new methods to property model

--- property_management_ui/lib/models/property.dart
+++ property_management_ui/lib/models/property.dart
@@ -4,5 +4,28 @@ class Property {
   String type;
   String image;
 
-  Property({required this.name, required this.address, required this.type, required this.image});
+  Property({
+    required this.name,
+    required this.address,
+    required this.type,
+    required this.image,
+  });
+
+  factory Property.fromJson(Map<String, dynamic> json) {
+    return Property(
+      name: json['name'] as String,
+      address: json['address'] as String,
+      type: json['type'] as String,
+      image: json['image'] as String,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'address': address,
+      'type': type,
+      'image': image,
+    };
+  }
 }
