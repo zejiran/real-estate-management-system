3cb3dc7701bfe5fad72ff576917594876282d914
Juan Alegría <juanszalegria@gmail.com>
2023-11-02 14:58:03 -0500

    Feat: add property model

---
+++ property_management_ui/lib/models/property.dart
@@ -0,0 +1,8 @@
+class Property {
+  String name;
+  String address;
+  String type;
+  String image;
+
+  Property({required this.name, required this.address, required this.type, required this.image});
+}
