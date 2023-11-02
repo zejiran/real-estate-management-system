676ffead7b2825fee20fb089abfc79bf9ccff302
Juan Alegría <juanszalegria@gmail.com>
2023-11-02 13:15:13 -0500

    Feat: create api handler for countries endpoint

---
+++ backend/api_handler.py
@@ -0,0 +1,58 @@
+from difflib import SequenceMatcher
+from flask import request, jsonify
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_TOKEN = os.getenv('API_TOKEN')
+USER_EMAIL = os.getenv('USER_EMAIL')
+BASE_URL = "https://www.universal-tutorial.com/api"
+
+
+def get_auth_token():
+    url = f"{BASE_URL}/getaccesstoken"
+    headers = {
+        "Accept": "application/json",
+        "api-token": API_TOKEN,
+        "user-email": USER_EMAIL
+    }
+    response = requests.get(url, headers=headers)
+    auth_token = response.json()["auth_token"]
+    return auth_token
+
+
+def get_countries():
+    country_name = request.args.get('name')
+    countries = fetch_countries()
+    filtered_countries = [country for country in countries if similarity(country_name, country['country_name']) > 0.8]
+    if any(c['country_name'] == country_name for c in filtered_countries):
+        regions = fetch_regions(country_name)
+        return jsonify({'regions': regions})
+    else:
+        return jsonify(filtered_countries)
+
+
+def fetch_countries():
+    url = f"{BASE_URL}/countries/"
+    headers = {
+        "Authorization": f"Bearer {get_auth_token()}",
+        "Accept": "application/json"
+    }
+    response = requests.get(url, headers=headers)
+    return response.json()
+
+
+def similarity(a, b):
+    return SequenceMatcher(None, a.lower(), b.lower()).ratio()
+
+
+def fetch_regions(country_name):
+    url = f"{BASE_URL}/states/{country_name}"
+    headers = {
+        "Authorization": f"Bearer {get_auth_token()}",
+        "Accept": "application/json"
+    }
+    response = requests.get(url, headers=headers)
+    return response.json()
--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,9 @@
 from flask import Flask
-from src.api_handler import get_countries, get_regions
+from api_handler import get_countries
 
 app = Flask(__name__)
 
 app.add_url_rule('/countries', view_func=get_countries, methods=['GET'])
-app.add_url_rule('/regions', view_func=get_regions, methods=['GET'])
 
 if __name__ == '__main__':
     app.run(debug=True)
